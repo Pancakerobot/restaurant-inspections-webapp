bd412a9ea3936503cda652aa51ef2c958dc8ae8f
Marcos Hernández <mhernandez@blim.com>
2016-08-15 20:45:42 -0400

    REmove css from the component

--- src/ts/counties/counties.component.ts
+++ src/ts/counties/counties.component.ts
@@ -1,38 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { County } from './county.model';
 import { CountiesService } from './counties.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-    selector: 'counties',
-    template: `
+  selector: 'counties',
+  template: `
         <table>
             <tr><th colspan="4">Counties</th>
             <tr><th>District</th><th>Number</th><th>Name</th><th>Inspections</th></tr>
-            <tr *ngFor="let county of counties"><td>{{ county.district }}</td><td>{{ county.countynumber }}</td><td>{{ county.countyname }}</td><td>{{ county.inspections }}</td>
+            <tr *ngFor="let county of counties" (click)=selectCounty(county)><td>{{ county.district }}</td><td>{{ county.countynumber }}</td><td>{{ county.countyname }}</td><td>{{ county.inspections }}</td>
         </table>
     `,
-    styles: [`
-        table {
-            margin: 1em;
-        }
-        th {
-            color: #eee;
-            background-color: #222;
-        }
-        td {
-            color: #222;
-            background-color: #eee;
-        }
-    `],
-    providers: [CountiesService]
+  styleUrls: ['src/css/counties.component.css'],
+  providers: [CountiesService]
 })
-export class CountiesComponent implements OnInit{
-    counties: County[];
-    constructor(private countiesService: CountiesService) {}
+export class CountiesComponent implements OnInit {
+  counties: County[];
+  selectedCounty: County;
+  constructor(private countiesService: CountiesService, private router: ActivatedRoute) { }
 
-    ngOnInit() {
-        this.countiesService.getCounties()
-            .subscribe(counties => this.counties = counties);
-    }
+  ngOnInit() {
+    this.countiesService.getCounties()
+      .subscribe(counties => this.counties = counties);
+  }
+
+  selectCounty(county) {
+    this.selectedCounty = county;
+    let link = ['/', this.selectedCounty.countynumber]
+
+    // this.router.navigate(link);
+  }
 
 }
